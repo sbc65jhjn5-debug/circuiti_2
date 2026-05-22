e5cecc126eae94fd05c2a9ba2f9154b694fe9886
Valeria Scarabelli <v.scarabelli@icloud.com>
2026-05-22 11:40:43 +0200

    modifiche

---
+++ retto.py
@@ -0,0 +1,14 @@
+# RETTIFICATORE DI CORRENTE
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+
+    stocazzo
+    # non ce niente da dire
+
+    # distanza tra 0 e max : 5 V
+    # distanza tra due massimi (verticale) : 10 V 
+    # differenza tra i due segnali varia da 4.20 a 4.40 V 
+    # godiamo perchè è tipo metà. Quello che manca per la metà è stato ciucciato dai dildi
